dcf71890d5af34326214c94da69a5be56f5bd0cb
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2025-06-28 05:42:14 +0500

    feat: init backend

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors({
+    origin:"*"
+}));
+app.use(express.json());
+
+// Sample route
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
